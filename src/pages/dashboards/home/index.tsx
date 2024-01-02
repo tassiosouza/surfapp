8e4321a320c6fb030c61ccfed9f742ac8407e796
Tassio de Souza Silva (CESAR) <tassio.souza@dmz.org.br>
2024-01-01 16:16:55 -0800

    small layout adjustments

--- src/pages/dashboards/home/index.tsx
+++ src/pages/dashboards/home/index.tsx
@@ -25,7 +25,7 @@ const HomeDashboard: FC<PageCollectionProps> = ({ className = "" }) => {
       </Helmet> */}
 
 
-      <div className="container py-16 lg:pb-28 lg:pt-20 space-y-16 sm:space-y-20 lg:space-y-28">
+      <div className="lg:pb-28 lg:pt-0 space-y-16 sm:space-y-20 lg:space-y-28">
         <div className="space-y-10 lg:space-y-14">
 
           <div className="max-w-screen-sm">
